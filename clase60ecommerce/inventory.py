94b8b160c8da234ab4ab6f682748fe095e6ff723
Leandro <90805322+Leandronolosopor@users.noreply.github.com>
2025-07-08 19:27:00 -0300

    Add files via upload

---
+++ clase60ecommerce/inventory.py
@@ -0,0 +1,9 @@
+#agrega un producto al inventario
+def add_product(produc_name, stock):
+    print(f'Producto {produc_name} agregando con {stock} unidades.')
+
+#elimina un producto del inventario
+def remove_product(product_name):
+    print(f'Producto {product_name} elminado del inventario')
+
+#Con estas 2 funciones estamos simulando que agregamos un produto y lo eliminamos, y lo simulamos haciendolo con un print
